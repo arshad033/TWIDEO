78e5815bc442be52ee0582602189b75094a26504
arshad033 <arshadobra85@gmail.com>
2025-03-07 12:38:39 +0530

    created delete files on cloudinary function

--- Backend/src/utils/cloudinary.js
+++ Backend/src/utils/cloudinary.js
@@ -20,11 +20,22 @@ export const uploadFileOnCloudinary = async (localFilePath) => {
     const response = await cloudinary.uploader.upload(localFilePath, {
       resource_type: "auto",
     });
-    console.log("file uploaded succesfully ", response.url);
+    // console.log("file uploaded succesfully ", response.url);
     fs.unlinkSync(localFilePath);
     return response;
   } catch (error) {
     fs.unlinkSync(localFilePath); // remove the locally saved temporary file as the upload operation got failed
+    return null;
+  }
+};
+export const deleteFileOnCloudinary = async (publicId, resourceType) => {
+  try {
+    if (!publicId) return null;
+    const response = await cloudinary.uploader.destroy(publicId, {
+      resource_type: resourceType,
+    });
+    return response;
+  } catch (error) {
     return null;
   }
 };
