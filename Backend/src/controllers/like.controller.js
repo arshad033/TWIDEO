97fd1ebe73a7c2a5caacb94ab157e5a11358fe70
arshad033 <arshadobra85@gmail.com>
2025-04-02 11:11:52 +0530

    modified the like controller

--- Backend/src/controllers/like.controller.js
+++ Backend/src/controllers/like.controller.js
@@ -7,65 +7,131 @@ import { ApiError } from "../utils/ApiError.js";
 import { ApiResponse } from "../utils/ApiResponse.js";
 
 //user can like the tweet
-const likeTweet = asyncHandler(async (req, res) => {
+const toggleTweetLike = asyncHandler(async (req, res) => {
   const { tweetId } = req.params;
   if (!tweetId) {
-    throw new ApiError(404, "Tweet id not found");
+    throw new ApiError(400, "Tweet id needed");
   }
   const tweet = await Tweet.findById(tweetId);
-  console.log("Tweet founded: ", tweet);
+  // console.log("Tweet founded: ", tweet);
   if (!tweet) {
-    throw new ApiError(400, "Tweet not found");
+    throw new ApiError(404, "Tweet not found");
   }
 
   const userId = req.user?._id;
-  const like = await Like.create({
-    likedBy: userId,
-    tweet: tweetId,
-  });
-  res.status(200).json(new ApiResponse(200, {}, "tweet liked succesfully"));
+  const alreadyLiked = await Like.findOne({ tweet: tweetId, likedBy: userId });
+  if (!alreadyLiked) {
+    const like = await Like.create({
+      likedBy: userId,
+      tweet: tweetId,
+    });
+    if (!like) {
+      throw new ApiError(500, "issue in liking the tweet");
+    }
+    res
+      .status(201)
+      .json(new ApiResponse(201, like, "tweet liked successfully"));
+  } else {
+    const deleteLike = await Like.findOneAndDelete({
+      tweet: tweetId,
+      likedBy: userId,
+    });
+    if (!deleteLike) {
+      throw new ApiError(500, "Issue in unliking the tweet");
+    }
+    res
+      .status(200)
+      .json(new ApiResponse(200, {}, "tweet unliked successfully"));
+  }
 });
 //user can like the video
-const likevideo = asyncHandler(async (req, res) => {
+const toggleVideoLike = asyncHandler(async (req, res) => {
   const { videoId } = req.params;
   if (!videoId) {
-    throw new ApiError(400, "Video id not found");
+    throw new ApiError(400, "video id needed");
   }
   const video = await Video.findById(videoId);
-  // console.log("Tweet founded: ", video);
+
   if (!video) {
     throw new ApiError(400, "video not found");
   }
 
   const userId = req.user?._id;
-  const like = await Like.create({
-    likedBy: userId,
-    video: videoId,
-  });
-  res.status(200).json(new ApiResponse(200, {}, "video liked succesfully"));
+  const alreadyLiked = await Like.findOne({ video: videoId, likedBy: userId });
+  if (!alreadyLiked) {
+    const like = await Like.create({
+      likedBy: userId,
+      video: videoId,
+    });
+    if (!like) {
+      throw new ApiError(500, "issue in liking the video");
+    }
+    res
+      .status(201)
+      .json(new ApiResponse(201, like, "video liked successfully"));
+  } else {
+    const deleteLike = await Like.findOneAndDelete({
+      video: videoId,
+      likedBy: userId,
+    });
+    if (!deleteLike) {
+      throw new ApiError(500, "issue in unliking the video");
+    }
+    res
+      .status(200)
+      .json(new ApiResponse(200, {}, "video unliked successfully"));
+  }
 });
 //user can like the tweet
-const likecomment = asyncHandler(async (req, res) => {
+const toggleCommentLike = asyncHandler(async (req, res) => {
   const { commentId } = req.params;
   if (!commentId) {
-    throw new ApiError(400, "Tweet id not found");
+    throw new ApiError(404, "Tweet id not found");
   }
   const comment = await Comment.findById(commentId);
-  // console.log("Tweet founded: ", comment);
+
   if (!comment) {
-    throw new ApiError(404, "comment not found");
+    throw new ApiError(400, "comment not found");
   }
 
   const userId = req.user?._id;
-  const like = await Like.create({
-    likedBy: userId,
+  const alreadyLiked = await Like.findOne({
     comment: commentId,
+    likedBy: userId,
   });
-  res.status(200).json(new ApiResponse(200, {}, "Comment liked succesfully"));
+  if (!alreadyLiked) {
+    const like = await Like.create({
+      likedBy: userId,
+      comment: commentId,
+    });
+    if (!like) {
+      throw new ApiError(500, "issue in liking the comment");
+    }
+    res
+      .status(201)
+      .json(new ApiResponse(201, like, "comment liked successfully"));
+  } else {
+    const deleteLike = await Like.findOneAndDelete({
+      comment: commentId,
+      likedBy: userId,
+    });
+    if (!deleteLike) {
+      throw new ApiError(500, "issue in unliking the comment");
+    }
+    res
+      .status(200)
+      .json(new ApiResponse(200, {}, "Comment unlike successfully"));
+  }
 });
 
-export default {
-  likeTweet,
-  likevideo,
-  likecomment,
-};
+//write the pipeline to get the liked videos
+const getLikedVideos = asyncHandler(async (req, res) => {
+  const userId = req.user?._id;
+  const likedVideos = await Like.find({ likedBy: userId });
+  res
+    .status(200)
+    .json(
+      new ApiResponse(200, likedVideos, "liked videos fetched successfully")
+    );
+});
+export { getLikedVideos, toggleTweetLike, toggleVideoLike, toggleCommentLike };
